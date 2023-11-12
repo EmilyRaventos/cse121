6186659cf3a0ac270b5cd3b918b617b83866e9e8
Emily Raventos <rav18001@byui.edu>
2023-11-11 18:32:42 -0700

    programming tasks

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -3,27 +3,38 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+let fullName = "Emily Raventos";
+let currentYear = "2023";
+// let profilePicture = "images/profilePic.jpg";
+let profilePicture = "w02-task/images/profilePic.jpg";
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector(
+  'img[src="images/placeholder.png"]'
+);
+imageElement.src = profilePicture;
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+imageElement.setAttribute("src", "/images/profilePic.jpg");
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
+const favFoods = ["pizza", " icecream", " cereal", " salads", " sandwiches"];
+foodElement.innerHTML = `<br>${favFoods}`;
 
+// Add a favorite food to the list.
+const favoriteFood = " enchiladas";
+favFoods.push(favoriteFood);
+foodElement.innerHTML += `<br>${favFoods}`;
 
+// Remove the first food from the list.
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
 
-
-
-
+// Remove the last food from the list.
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
